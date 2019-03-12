aba7b8f6ff88945991e1c6290bc22150f37fd850
tristan <yangguochun@ddweilai.com>
2019-03-12 18:15:51 +0800

    Updates

---
+++ precache-manifest.9e9987fc60c598d5c1ea544285d2a083.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "552a87abb58bf8a20317",
+    "url": "/react-h5/static/js/main.552a87ab.chunk.js"
+  },
+  {
+    "revision": "715a9c8273be36d3d9bf",
+    "url": "/react-h5/static/js/runtime~main.715a9c82.js"
+  },
+  {
+    "revision": "2475e5df217e71e95af5",
+    "url": "/react-h5/static/js/2.2475e5df.chunk.js"
+  },
+  {
+    "revision": "197a38c22f41f9f8fe44a4497af31e4b",
+    "url": "/react-h5/static/media/airplane.197a38c2.png"
+  },
+  {
+    "revision": "63333ed7c40f3d66572d584ddbe1bc6a",
+    "url": "/react-h5/static/media/spider.web.63333ed7.svg"
+  },
+  {
+    "revision": "1ee4ac317ed625042924d6d737df8aef",
+    "url": "/react-h5/index.html"
+  }
+];
